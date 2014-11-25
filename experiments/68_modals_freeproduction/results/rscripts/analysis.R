060fd9914f186efe7ad001ae09bf4fd3a5abf812
thegricean <judithdegen@gmail.com>
2014-11-25 12:06:11 -0800

    added some plots and analyses

---
+++ experiments/68_modals_freeproduction/results/rscripts/analysis.R
@@ -0,0 +1,25 @@
+library(ggplot2)
+theme_set(theme_bw(18))
+setwd("~/webprojects/68_modals_freeproduction/results/")
+source("rscripts/helpers.r")
+load("data/r.RData")
+
+## analysis of probability of p for SALT abstract
+baremust = subset(r, item_type %in% c("bare","must"))
+nrow(baremust)
+centered = cbind(baremust, myCenter(baremust[,c("item_type","item")]))
+
+m = lmer(response ~ citem_type + (1|item) + (1|workerid),data=centered)
+summary(m)
+
+m = lmer(response ~ item_type + (1|item) + (1|workerid),data=r)
+summary(m)
+
+
+## analysis of evidence for SALT abstract
+baremust = subset(r, item_type %in% c("bare","must") & !red_evidence_type_greg %in% c("meta","uncertain"))
+nrow(baremust)
+baremust = droplevels(baremust)
+baremust$EvidenceSource = as.factor(ifelse(baremust$red_evidence_type_greg == "perceptual","direct","indirect"))
+m = glmer(EvidenceSource ~ item_type + (1|item) + (1|workerid),data=baremust,family="binomial")
+summary(m)
